095b1f4e0f682093df2cb29077631aec904cac6f
Ojasp21 <ojasp21@gmail.com>
2025-05-03 18:33:44 +0530

    added navigation to logo

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useSAuthStore } from '../store/useSAuthStore';
 import { useTAuthStore } from '../store/useTAuthStore';
 import { LogOut, User } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const { logout: studentLogout, authUser: studentAuthUser } = useSAuthStore();
     const { logout: teacherLogout, authUser: teacherAuthUser } = useTAuthStore();
-
+    const navigate = useNavigate();
     const handleLogout = () => {
         if (studentAuthUser) {
             studentLogout();
@@ -17,13 +18,21 @@ const Navbar = () => {
     };
 
     const userName = studentAuthUser?.fullName || teacherAuthUser?.fullName || ""; 
-
+ 
     return (
         <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
             <div className="container mx-auto px-4 h-16">
                 <div className="flex items-center justify-between h-full">
                     <div className="flex items-center gap-8">
-                        <h1 className="text-lg font-bold">ACGC</h1>
+                        <h1 className="text-lg font-bold" onClick={() => {
+                            if(studentAuthUser){
+                                navigate("/studentDash");
+                            }else if(teacherAuthUser){
+                                navigate("/teacherDash");
+                            }else{
+                                navigate("/");
+                            }
+                        }}>ACGC</h1>
                     </div>
 
                     <div className="flex items-center gap-4">
